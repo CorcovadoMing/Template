7f1a4562861560448a512d83b6e988d9dec858f6
CorcovadoMing <rf37535@gmail.com>
2016-02-20 01:10:11 +0800

    Add user in docker container

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp Dockerfile.template Dockerfile
-echo "RUN adduser --disabled-password --gecos '' $(whoami)" >> Dockerfile
+cat Dockerfile.template | sed "7 i RUN useradd -m $(whoami) && echo $(whoami):$(whoami) | chpasswd && adduser $(whoami) sudo\nRUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers\nWORKDIR /home/$(whoami)\nUSER $(whoami)" >> Dockerfile
 docker rmi util/template
 docker build -t util/template .
+rm Dockerfile
